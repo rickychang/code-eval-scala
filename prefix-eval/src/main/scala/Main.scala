df91ed91a29d38a9d77a230dded12b2e9f01b2e9
Ricky Chang <ricky.m.chang@gmail.com>
2013-07-19 15:52:39 -0500

    cleaning up variable names

--- prefix-eval/src/main/scala/Main.scala
+++ prefix-eval/src/main/scala/Main.scala
@@ -1,35 +1,40 @@
 import scala.collection.mutable.Stack
 
 object Main extends App {
-  
-  val operators = Map(
-      "+" -> { (x: Int, y: Int) => x + y },
-      "-" -> { (x: Int, y: Int) => x - y },
-      "*" -> { (x: Int, y: Int) => x * y },
-      "/" -> { (x: Int, y: Int) => x / y })
-  
-  def isOperator(s: String): Boolean = operators contains s
-  
-  def eval(prefixStr: String): Int = {
+
+  val tokenToOperator = Map(
+    "+" -> { (x: Int, y: Int) => x + y },
+    "-" -> { (x: Int, y: Int) => x - y },
+    "*" -> { (x: Int, y: Int) => x * y },
+    "/" -> { (x: Int, y: Int) => x / y })
+
+  def isOperator(tok: String) = tokenToOperator contains tok
+
+  def eval(expr: String): Int = {
     val stack = new Stack[Int]
-    val reversed = prefixStr.split(" ").reverse
-    for (v <- reversed) {
-      if (isOperator(v)) {
+    val reversed = expr.split(" ").reverse
+
+    for (t <- reversed) {
+      if (isOperator(t)) {
+        // if to token is operator, pop both operands from stack,
+        // evaluate, and push result back on stack
         val op1 = stack.pop
         val op2 = stack.pop
-        val operator = operators(v)
-        val result = operator(op1, op2)
-        stack.push(result)
+        val operator = tokenToOperator(t)
+        stack.push(operator(op1, op2))
       } else {
-        stack.push(v.toInt)
+        // otherwise assume token is valid operand
+        // and push it on to stack
+        stack.push(t.toInt)
       }
     }
+    // final result is on top of stack
     stack.pop
   }
 
   val source = scala.io.Source.fromFile(args(0))
   val lines = source.getLines.filter(_.length > 0)
   for (l <- lines) {
-   	println(eval(l))
+    println(eval(l))
   }
 }
