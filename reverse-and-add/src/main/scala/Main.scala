af52977362d603768e3575b04774cbd1aae44b49
Ricky Chang <ricky.m.chang@gmail.com>
2013-07-20 10:53:46 -0500

    minor fix

--- reverse-and-add/src/main/scala/Main.scala
+++ reverse-and-add/src/main/scala/Main.scala
@@ -3,20 +3,21 @@ import scala.annotation.tailrec
 object Main extends App {
   
   def isPal(num: Int): Boolean = num.toString == num.toString.reverse
-  
+
   @tailrec
   def reverseAddTest(count: Int, num: Int): (Int, Int) = {
-    val numReverse = num.toString.reverse.toInt
-    val sum = num + numReverse
-    if (isPal(sum)) (count + 1, sum)
-    else reverseAddTest(count + 1, sum)
+    if (isPal(num)) (count, num)
+    else {
+      val numReverse = num.toString.reverse.toInt
+      val sum = num + numReverse
+      reverseAddTest(count + 1, sum)
+    }
   }
   
   val source = scala.io.Source.fromFile(args(0))
   val lines = source.getLines.filter(_.length > 0)
   for (l <- lines) {
-    val (count, num) = reverseAddTest(0, l.toInt)
-    println(count + " " + num)
+    val result = reverseAddTest(0, l.toInt)
+    println(result._1 + " " + result._2)
   }
-
 }
